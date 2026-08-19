1b6d82b86997332bf3cefb1ab82a9027076563c6
Brie Lopez <Brie.Lopez@ttu.edu>
2026-08-18 19:29:37 -0500

    Update pageTitle in quartz.config.ts
    updated page title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "📖 Knowledge Base Docs",
+    pageTitle: "📖 pageTitle: "ShutterStories"",
     enableSPA: true,
     enablePopovers: true,
     analytics: null,
